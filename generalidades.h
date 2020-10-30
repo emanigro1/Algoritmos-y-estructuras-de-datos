9472fbd9bdff2ac8e561ccef7cbebbd6a4f1a23d
Rolo-A <72111508+Rolo-A@users.noreply.github.com>
2020-10-30 00:35:25 -0300

    Add files via upload

--- generalidades.h
+++ generalidades.h
@@ -4,6 +4,25 @@
 #include "ListaSucursales.h"
 #include "TDA.h"
 
+
+/**
+ Definicion del Tipo de Dato para manejo del Rendimiento
+ Atributos:
+ * codSuc
+ * casaMatriz
+ * rendimiento
+
+ Axiomas:
+ * codSuc tiene que poseer 4 chars
+ * casaMatriz tiene que poseer 4 chars
+ * rendimiento > 0
+ */
+
+ /*******************************************************************/
+/**  Definiciones de Tipos de Datos   */
+/**-----------------------------------*/
+
+/** Tipo de Estructura del Rendimiento */
 typedef struct
 {
     char codSuc[5];
@@ -21,14 +40,39 @@ typedef struct
 */
 void cargarDatos(Listaenc* miLista);
 /*------------------------------------------------------------------------------------*/
+/**
+    PRE : La lista debe haber sido creada y debe haber sido cargada con cargarDatos()
+    POST: Devuelve una lista ordenada de Nacional mayor a menor en base al campo MontoMensual
+    lista: Instancia sobre la cual se invoca la primitiva
+*/
 void ordenarNacionalFacturacion(Listaenc* miLista);
 /*------------------------------------------------------------------------------------*/
+/**
+    PRE : La lista debe haber sido creada y debe haber sido cargada con cargarDatos()
+    POST: Devuelve una lista ordenada de Provincial mayor a menor en base al campo MontoMensual
+    lista: Instancia sobre la cual se invoca la primitiva
+*/
 void ordenarProvinciaFacturacion(Listaenc* miLista);
 /*------------------------------------------------------------------------------------*/
+/**
+    PRE : La lista debe haber sido creada y debe haber sido cargada con cargarDatos()
+    POST: Devuelve una lista ordenada de Nacional mayor a menor en base al campo Cantidad de Articulos
+    lista: Instancia sobre la cual se invoca la primitiva
+*/
 void ordenarNacionalCantArt(Listaenc* miLista);
 /*------------------------------------------------------------------------------------*/
+/**
+    PRE : La lista debe haber sido creada y debe haber sido cargada con cargarDatos()
+    POST: Devuelve una lista ordenada de Provincial mayor a menor en base al campo Cantidad de Articulos
+    lista: Instancia sobre la cual se invoca la primitiva
+*/
 void ordenarProvinciaCantArt(Listaenc* miLista);
 /*------------------------------------------------------------------------------------*/
+/**
+    PRE : La lista debe haber sido creada y debe haber sido cargada con cargarDatos()
+    POST: Devuelve una lista ordenada mayor a menor en base al Rendimiento de las casa matrices
+    lista: Instancia sobre la cual se invoca la primitiva
+*/
 void ordenarPorRendimiento(Listaenc* miLista);
 /*------------------------------------------------------------------------------------*/
 
